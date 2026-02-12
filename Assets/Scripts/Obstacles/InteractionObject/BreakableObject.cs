bcf494eacd5ae111b91753b311a10d7719cd97d7
jwchoi <juchoi0632@gmail.com>
2026-02-12 14:10:37 +0900

    [Add] BreakableObject
    공격이 Breakable 속성을 가지면 피격 시 부숴지는 BreakableObject 구현

---
+++ Assets/Scripts/Obstacles/InteractionObject/BreakableObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BreakableObject : MonoBehaviour, IHitable, IInteractable
+{
+    [SerializeField] protected InteractionType _interactionType;
+
+    public HitState _hitState { get; private set; }
+    public HitDatabase HitData { get; private set;}
+
+    public InteractionType SupportedType => _interactionType;
+
+    public bool CanInteraction(GameObject casuer, InteractionType type)
+    {
+        if (type == InteractionType.Breakable) return true;
+
+        return false;
+    }
+
+    public void DecreaseHp(float decreaseValue) { }
+
+    public void IncreaseHp(float increaseValue) { }
+
+    public void OnInteraction(GameObject casuer, InteractionType type, Vector2 force = default)
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void PostHit() { }
+
+    public void TakeDamage(HitInfo hitInfo)
+    {
+        OnInteraction(hitInfo.causer, _interactionType);
+    }
+}
